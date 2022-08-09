a96c5f74fcedb09d6010510440b7d1801b6b12d6
dragon-fire-fly <ek93@hotmail.co.uk>
2022-08-09 13:48:34 +0200

    Complete exercises 1 and 2

---
+++ exercise.py
@@ -0,0 +1,42 @@
+# do exercise here
+# upload to github for portfolio
+# thank me later when your git graph is as green as the python money u gonna earn
+
+
+#----# Exercise 1 #----#
+
+# Integer
+print(1234)
+
+# Float
+print(1.234)
+
+# Complex
+print(1 + 1j)
+
+# String
+print("Hello world!")
+
+# Boolean
+print(True)
+
+#----# Exercise 2 #----#
+
+# Integer
+integer1 = 1234
+print(str(integer1) + " is type of " + str(type(integer1)))
+# Float
+float1 = 1.234
+print(str(float1) + " is type of " + str(type(float1)))
+
+# Complex
+complex1 = 1 + 1j
+print(str(complex1) + " is type of " + str(type(complex1)))
+
+# String
+string1 = "Hello world!"
+print(str(string1) + " is type of " + str(type(string1)))
+
+# Boolean
+boolean1 = True
+print(str(boolean1) + " is type of " + str(type(boolean1)))
